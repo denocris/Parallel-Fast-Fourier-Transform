58895263250a72a402043f29c0c2c23c8761875f
denocris <denobili16@gmail.com>
2017-05-10 10:02:21 +0200

    mod script short

--- D1-exercise1/solution/C/request_short.sh
+++ D1-exercise1/solution/C/request_short.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=20 -q regular -l walltime=00:05:00
+#PBS -l nodes=1:ppn=20 -q regular -l walltime=00:01:00
 module load openmpi/1.8.3/gnu/4.9.2
 module load fftw/3.3.4/gnu/4.9.2
 
-cd P2.8_seed/D1-exercise1/solution/C
+cd /home/cdenobi/P2.8_seed/D1-exercise1/solution/C
+
+make
 
 processes="1 2 4 8 16"
 sizes="48"
 
-
 for size in $sizes; do
 outfile="log_strong$size.txt"
-
-echo "---------------------STRONG SCALABILITY---------------------" > $outfile
+#echo "---------------------STRONG SCALABILITY---------------------" > $outfile
 	for p in $processes; do
 		#p=$(($node * 16))
 		echo "-------------NUMBER OF PROCESSOR=$p-----------------" >> $outfile
-		mpirun -np $p --map-by ppr:16:node:pe=1 diffusion.x $size >> $outfile
+		mpirun -np $p --map-by ppr:16:node:pe=1 ./diffusion.x $size >> $outfile
 
 	done
 
