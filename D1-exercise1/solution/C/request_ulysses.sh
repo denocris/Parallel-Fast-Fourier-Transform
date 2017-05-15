9963d260a89199388239ac1053ebde2169dd2a21
denocris <denobili16@gmail.com>
2017-05-15 19:11:51 +0200

    mod ulysses

--- D1-exercise1/solution/C/request_ulysses.sh
+++ D1-exercise1/solution/C/request_ulysses.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-#PBS -l nodes=16:ppn=20 -q wide -l walltime=00:05:00
+#PBS -l nodes=16:ppn=20 -q wide -l walltime=00:10:00
+#PBS -N fft
 
 module load openmpi
 module load fftw/3.3.4
 
-cd P2.8_seed/D1-exercise1/solution/C
+cd /home/cdenobi/P2.8_seed/D1-exercise1/solution/C
 #cd $PBS_O_WORKDIR
 
+make
+
 num_nodes="1 2 4 8 16"
-dimension="64"
+dimension="128"
+
+
+outfile="log_strong$size.txt"
+
 
 for size in $dimension; do
   for node in $num_nodes; do
     nproc=$(($node * 8))
-    mpirun -np $nproc --map-by ppr:8:node:pe=1 ./diffusion.x $size >> strong_$size.txt
+    echo "------------- NUMBER OF PROCESSOR = $nproc -----------------" >> $outfile
+    mpirun -np $nproc --map-by ppr:8:node:pe=1 ./diffusion.x $size >> $outfile
   done
 done
