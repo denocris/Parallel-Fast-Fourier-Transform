60f36ffbfced8fede57fd4bda4d05b004795c4e0
denocris <denobili16@gmail.com>
2017-05-17 17:56:06 +0200

    mod script

--- D1-exercise1/solution/C/request_ulysses.sh
+++ D1-exercise1/solution/C/request_ulysses.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#PBS -l nodes=16:ppn=20 -q wide -l walltime=00:10:00
+#PBS -l nodes=16:ppn=20 -q wide -l walltime=00:30:00
 #PBS -N fft
 
 module load openmpi
 module load fftw/3.3.4
 
-cd /home/cdenobi/P2.8_seed/D1-exercise1/solution/C
+cd /home/cdenobi/P2.8_seed/D2-exercise2/provided_code/myC
 #cd $PBS_O_WORKDIR
 
 make
-
+# do
 num_nodes="1 2 4 8 16"
-sizes="128"
+sizes="128 512"
 
 
 outfile="log_strong$size.txt"
@@ -19,8 +19,24 @@ outfile="log_strong$size.txt"
 
 for size in $sizes; do
   for node in $num_nodes; do
-    nproc=$(($node * 8))
+    nproc=$(($node * 16))
     echo "------------- NUMBER OF PROCESSOR = $nproc -----------------" >> $outfile
-    mpirun -np $nproc --map-by ppr:8:node:pe=1 ./diffusion.x $size >> $outfile
+    mpirun -np $nproc --map-by ppr:16:node:pe=1 ./diffusion.x $size >> $outfile
+  done
+done
+
+
+num_nodes="16"
+sizes="2048"
+
+
+outfile="log_strong$size.txt"
+
+
+for size in $sizes; do
+  for node in $num_nodes; do
+    nproc=$(($node * 16))
+    echo "------------- NUMBER OF PROCESSOR = $nproc -----------------" >> $outfile
+    mpirun -np $nproc --map-by ppr:16:node:pe=1 ./diffusion.x $size >> $outfile
   done
 done
